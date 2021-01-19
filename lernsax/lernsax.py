4cbbd7c409fba2e6ed201897742bdc7058017f1b
Léon Becker <lb@alien8.de>
2021-01-20 00:09:32 +0100

    Updated type annotations

--- lernsax/lernsax.py
+++ lernsax/lernsax.py
@@ -26,59 +26,59 @@ class Client:
     def post(self, json) -> dict:
         return requests.post(self.api, json=json).json()
 
-    def login(self, email: str, password: str) -> list:
+    def login(self, email: str, password: str) -> dict:
         return login_to_lernsax(self, email, password)
 
-    def refresh_session(self) -> list:
+    def refresh_session(self) -> dict:
         return refresh_lernsax_session(self)
 
-    def logout(self) -> list:
+    def logout(self) -> dict:
         return logout_from_lernsax(self)
 
-    def get_state(self, login: str) -> list:
+    def get_state(self, login: str) -> dict:
         return get_storage_state(self, login)
 
-    def get_files(self, login: str, recursive: bool) -> list:
+    def get_files(self, login: str, recursive: bool) -> dict:
         return get_lernsax_files(self, login, recursive)
 
-    def get_tasks(self) -> list:
+    def get_tasks(self) -> dict:
         return get_lernsax_tasks(self)
 
-    def get_notes(self, login: str) -> list:
+    def get_notes(self, login: str) -> dict:
         return get_lernsax_notes(self, login)
 
-    def get_board(self, login: str) -> list:
+    def get_board(self, login: str) -> dict:
         return get_lernsax_board(self, login)
 
-    def add_board_entry(self, login: str, title: str, text: str, color: str) -> list:
+    def add_board_entry(self, login: str, title: str, text: str, color: str) -> dict:
         return add_lernsax_board_entry(self, login, title, text, color)
 
-    def add_note(self, title: str, text: str) -> list:
+    def add_note(self, title: str, text: str) -> dict:
         return add_lernsax_note(self, title, text)
 
-    def delete_note(self, id: str) -> list:
+    def delete_note(self, id: str) -> dict:
         return delete_lernsax_note(self, id)
 
-    def get_email_folders(self) -> list:
+    def get_email_folders(self) -> dict:
         return get_lernsax_email_folders(self)
 
-    def get_emails(self, folder_id: str) -> list:
+    def get_emails(self, folder_id: str) -> dict:
         return get_lernsax_emails(self, folder_id)
 
-    def read_email(self, folder_id: str, message_id: int) -> list:
+    def read_email(self, folder_id: str, message_id: int) -> dict:
         return read_lernsax_email(self, folder_id, message_id)
 
-    def send_email(self, to: str, subject: str, body: str) -> list:
+    def send_email(self, to: str, subject: str, body: str) -> dict:
         return send_lernsax_email(self, to, subject, body)
 
-    def get_quickmessages(self) -> list:
+    def get_quickmessages(self) -> dict:
         return read_lernsax_quickmessages(self)
 
-    def get_quickmessage_history(self, start_id: int) -> list:
+    def get_quickmessage_history(self, start_id: int) -> dict:
         return get_lernsax_quickmessage_history(self, start_id)
 
-    def group_quickmessage_history_by_chat(self, quickmsg_history: list) -> list:
+    def group_quickmessage_history_by_chat(self, quickmsg_history: list) -> dict:
         return group_lernsax_quickmessage_history_by_chat(quickmsg_history)
 
-    def send_quickmessage(self, login: str, text: str) -> list:
+    def send_quickmessage(self, login: str, text: str) -> dict:
         return send_lernsax_quickmessage(self, login, text)
